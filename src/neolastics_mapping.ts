8e52933a24d8dbe4bcba859b224298c74b88c960
Simon de la Rouviere <simon@delarouviere.com>
2021-01-07 12:02:55 +0200

    added Transfer event to subgraph

--- src/neolastics_mapping.ts
+++ src/neolastics_mapping.ts
@@ -1,5 +1,5 @@
 import { BigInt, store, log } from "@graphprotocol/graph-ts"
-import { Minted, Burned } from '../generated/Neolastics/Neolastics'
+import { Minted, Burned, Transfer } from '../generated/Neolastics/Neolastics'
 import { Neolastic, Collector, Curve } from '../generated/schema'
 import { getNeolastic, getCurve, getCollector } from './helpers';
 
@@ -33,4 +33,30 @@ export function handleNewERC721Burned(event: Burned): void {
   collector.save()
   curve.save()
   store.remove('Neolastic', event.params.tokenId.toString())
+}
+
+export function handleTransfer(event: Transfer): void {
+  // NOTES:
+  // Transfer is fired on mint + burn.
+  // Mint from 0x0 to <mint_address>
+  // Burn from <owner> to 0x0.
+  // Thus: when changing ownership, only change it in the graph when not using the 0x0 address.
+  // You can't transfer 0x0 normally, so this won't fire otherwise.
+  if(event.params.from.toHexString() !== "0x0000000000000000000000000000000000000000" ||
+    event.params.to.toHexString() !== "0x0000000000000000000000000000000000000000") {
+    // first fetch collector in case they don't exist yet.
+    let newCollector = getCollector(event.params.to.toHexString())
+    newCollector.totalOwned = newCollector.totalOwned.plus(BigInt.fromI32(1)); 
+
+    // subtract oldCollector's stats
+    let oldCollector = getCollector(event.params.from.toHexString())
+    oldCollector.totalOwned = oldCollector.totalOwned.minus(BigInt.fromI32(1))
+
+    let neolastic = getNeolastic(event.params.tokenId.toString())
+    neolastic.owner = event.params.to.toHexString();
+
+    neolastic.save();
+    newCollector.save();
+    oldCollector.save();
+  }
 }
